783c20f0dc1dc34a47c3b890b8c7240b04836487
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-03-27 20:54:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "26751998"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "1fbc7e2e1185d0c6fd2092b09861d026"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7029942301"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "Noob_Coderzzz"
+OWNER_ID = "7110021757"
+SUPPORT_GRP = "AMRIT_X_SUPPORTS"
+UPDATE_CHNL = "AMRIT_X_SUPPORT"
+OWNER_USERNAME = "amrit_Koniki"
 
